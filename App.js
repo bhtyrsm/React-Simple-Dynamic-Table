6d9f59aeb0153a447cd17f53df9be54ca2f41c16
Bahtiyar <bahtiyarsime@gmail.com>
2020-05-28 20:38:41 +0300

    React-Table
    Simple reactjs dynamic table

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Table  from './modules/Table';
+
+function App() {
+  return (
+    <div>
+      <Table/>
+    </div>
+  );
+}
+
+export default App;
